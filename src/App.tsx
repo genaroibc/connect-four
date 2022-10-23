176bb06bd90cf8398d10e0b3e44195614d2e13f8
genaroibc <bonavitaismael@gmail.com>
2022-10-23 16:32:51 -0300

    feat: add main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Board } from "./components/Board/Board";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Board cols={8} rows={6} />
+    </>
+  );
 }
 
 export default App;
